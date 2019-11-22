1be8848b664b3ba533d249cb06b9199cf7c8e55f
arigatory <arigatory@gmail.com>
2019-11-22 21:47:03 +0300

    Add project files.

---
+++ dotNETtask5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace dotNETtask5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            Console.SetCursorPosition(0,10);
+            Console.Write("Помещаем числа в стек ---->");
+
+            Console.WriteLine(stack.Pop());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ dotNETtask5/Stack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Diagnostics.Eventing.Reader;
+
+namespace dotNETtask5
+{
+    public class Stack<T>
+    {
+        private Node<T> _first, _last;
+        public void Push(T data)
+        {
+            Node<T> temp = new Node<T>(data);
+            if (_last == null)
+            {
+                _last = temp;
+                _first = temp;
+            }
+            else
+            {
+                temp.Next = _first;
+                _first = temp;
+            }
+        }
+
+        public T Pop()
+        {
+            if (_last == null)
+            {
+                return default(T);
+            }
+
+            Node<T> temp;
+            if (_last == _first)
+            {
+                temp = _first;
+                _first = _last = null;
+                return temp.Data;
+            }
+
+            temp = _first;
+            _first = _first.Next;
+            return temp.Data;
+        }
+
+        public int Count()
+        {
+            int res = 0;
+            Node<T> temp = _first;
+            while (temp != null)
+            {
+                res++;
+                temp = temp.Next;
+            }
+            return res;
+        }
+
+        public T Top()
+        {
+            return _first.Data;
+        }
+
+        private class Node<TVal>
+        {
+            public Node(TVal data, Node<TVal> next = null)
+            {
+                Data = data;
+                Next = next;
+            }
+
+            public TVal Data { get; set; }
+            public Node<TVal> Next { get; set; }
+        }
+    }
+}
